68efc4ebf834f2cf196497ea01fbc399cdb5583c
chengkang <1206734994@qq.com>
2021-05-16 21:36:20 +0800

    first

---
+++ src/main/java/com/imnoob/App.java
@@ -0,0 +1,13 @@
+package com.imnoob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
